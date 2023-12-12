638202585f95d5d6ec83d5fb392601f9c47e9a71
Burak Çolak <burakc@outlook.com>
2023-12-12 14:50:10 +0300

    Discount calculate approach changed

--- src/ShopsRUs.Application/Discount/DiscountService.cs
+++ src/ShopsRUs.Application/Discount/DiscountService.cs
@@ -10,30 +10,26 @@ public class DiscountService : IDiscountService
         //Employee discount
         if (customer.IsEmployee)
         {
-            return CalculatePercentageDiscount(product, 30);
+            return CalculatePercentageDiscountedAmount(product, 30);
         }
 
         //Affiliate discount
         if (customer.IsAffiliate)
         {
-            return CalculatePercentageDiscount(product, 10);
+            return CalculatePercentageDiscountedAmount(product, 10);
         }
 
         //Customer loyalty discount (if customer for over 2 years)
         int customerAge = (DateTime.Now - customer.CreatedAt).Days;
         if (customerAge >= 365 * 2)
         {
-            return CalculatePercentageDiscount(product, 5);
+            return CalculatePercentageDiscountedAmount(product, 5);
         }
 
         return product.Price;
-        //// Rule 4: $5 discount for every $100 on the bill (excluding groceries)
-        //totalDiscount += CalculateFixedAmountDiscount(invoice);
-
-        //return totalDiscount;
     }
 
-    private decimal CalculatePercentageDiscount(Product product, decimal percentage)
+    private decimal CalculatePercentageDiscountedAmount(Product product, decimal percentage)
     {
         //The percentage based discounts do not apply on groceries.
         if (product.Category == Common.Enums.ProductCategory.Grocery)
@@ -41,7 +37,8 @@ public class DiscountService : IDiscountService
             return product.Price;
         }
 
-        return (product.Price * percentage) / 100;
+        decimal discountedAmountPercentage = 100 - percentage;
+        return (product.Price * discountedAmountPercentage) / 100;
     }
 
     public decimal CalculateDiscountedTotalAmount(decimal totalAmount)
