4341fa14186ced7f28cf2cee512f7d01d05495da
Burak Çolak <burakc@outlook.com>
2023-12-13 13:15:36 +0300

    Customer Response contract fix

--- src/ShopsRUs.Contracts/Public/Invoice/CreateInvoiceResponse.cs
+++ src/ShopsRUs.Contracts/Public/Invoice/CreateInvoiceResponse.cs
@@ -19,9 +19,5 @@ public class ProductResponse
 
 public class CustomerResponse
 {
-    public int Id { get; set; }
     public string Name { get; set; }
-    public bool IsEmployee { get; set; }
-    public bool IsAffiliate { get; set; }
-    public DateTime CreatedAt { get; set; }
 }
